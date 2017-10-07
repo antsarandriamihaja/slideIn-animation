32c1320d56833c292ef9750de7893cc8d39c5b5f
Antsa Randriamihaja <arandriamihaja@gmail.com>
2017-10-07 16:05:16 -0400

    alignment for aboutme

--- src/components/aboutMe/index.js
+++ src/components/aboutMe/index.js
@@ -3,7 +3,7 @@ import styles from 'styled-components';
 import profilePic from '../../assets/profilePic.jpg';
 
 const ContentWrapper = styles.div`
-display: flex;
+flex-direction: row;
 justify-content: center;
 margin: 0 auto;
 height: 100vh;
@@ -11,17 +11,20 @@ color: black;
 `;
 
 const ProfilePic = styles.div`
+margin: 0 auto;
+margin-top: 15%;
 background-image: url(${profilePic});
 background-repeat: no-repeat;
 background-size: cover;
-width: 200px;
-height: 200px;
+width: 300px;
+height: 300px;
 clear: right;
 border-radius: 50%;
 `;
 
 const Title = styles.h1`
-font-size: 3em;
+font-size: 2em;
+text-align: center;
 `;
 
 class AboutMe extends React.Component {
